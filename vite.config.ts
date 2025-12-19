181f949c365ed2bab4fec7304759b18a2012d300
AxelDoussoux <axeldoussoux@gmail.com>
2025-12-19 17:32:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/planning-poker/',
+  base: '/Conception-de-projet--Planning-Poker-Doussoux-Berger/',
   plugins: [
     react({
       babel: {
